e06f3daf78ce319cff5ad690cca2c9a2a107e5d6
RFS-ADRENO <77768272+RFS-ADRENO@users.noreply.github.com>
2021-11-22 18:56:48 +0700

    Update custom_message.js

--- app/handle/custom_message.js
+++ app/handle/custom_message.js
@@ -61,7 +61,9 @@ module.exports = function({
             if (!bodyPrefix) return api.sendMessage("Thiếu input!", threadID, messageID);
             prefixFile[threadID] = bodyPrefix;
             fs.writeFileSync(__dirname + '/src/prefix.json', JSON.stringify(prefixFile, null, 4));
-            return api.sendMessage(`Prefix mới của bot ở nhóm này là: ${config.threadPrefix[threadID]}`, threadID);
+            return api.sendMessage(`Prefix mới của bot ở nhóm này là: ${config.threadPrefix[threadID]}`, threadID, () => {
+                api.changeNickname(`[ ${config.threadPrefix[threadID]} ] ${config.BOTNAME}`, threadID, api.getCurrentUserID());
+            });
         }
 
         //Do code here
